e60286bd4a889d23e8e2acbf97302e2bb969374a
Felipe Carlos <efelipecarlos@gmail.com>
2026-07-05 01:58:42 -0300

    utils: build wheel script

--- scripts/build-wheels.bat
+++ scripts/build-wheels.bat
@@ -4,9 +4,11 @@ echo Building wheels
 
 REM Clean previous builds
 echo Cleaning previous builds...
+
 if exist build rmdir /s /q build
 if exist dist rmdir /s /q dist
 if exist *.egg-info rmdir /s /q *.egg-info
+
 for /d /r . %%d in (__pycache__) do @if exist "%%d" rmdir /s /q "%%d"
 for /r . %%f in (*.pyc) do @if exist "%%f" del /q "%%f"
 
@@ -16,6 +18,7 @@ python -m pip install --upgrade pip build wheel
 
 REM Build source distribution and wheel
 echo Building source distribution and wheel...
+
 python -m build
 
 echo.
